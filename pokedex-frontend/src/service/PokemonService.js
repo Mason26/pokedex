910dd80ef59043561ea21fd598e9cf93bdf7089e
Mason <michaeltmason26@gmail.com>
2024-02-16 19:23:55 +0200

    Added styling and some functionality

--- pokedex-frontend/src/service/PokemonService.js
+++ pokedex-frontend/src/service/PokemonService.js
@@ -1,78 +1,53 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-import React, { useLayoutEffect, useRef, useState } from "react";
-import PokeballLoadingSpinner from "../components/PokeballLoadingSpinner";
-import axios from "axios";
-import Button from "@mui/material/Button";
+// import axios from "axios";
 
-const api = axios.create({
-  baseURL: "https://pokeapi.co/api/v2/pokemon/",
-});
+// const api = axios.create({
+//   baseURL: "https://pokeapi.co/api/v2/pokemon/",
+// });
 
-// function goUpperCase({name}) {
-//   name.
+// export function PokemonService({ pokemon }) {
+// async function fetchPokemon() {
+//   let url = pokemon.toLowerCase();
+//   await api.get(url).then((res) => {
+//     setData(res.data);
+//     return res.data;
+//   });
+// }
+// async function fetchPokemonImage(resData) {
+//   await api.get(resData.forms[0].url).then((res) => {
+//     setImage(res);
+//     return res.data;
+//   });
+// }
+//   return (
+//     <div>
+//       {data ? (
+//         <div className="pokemon-card">
+//           <div className="pokemon-name">{data.name}</div>
+//           <div className="ability-container">
+//             {data.abilities.map((ability) => {
+//               console.log(ability);
+//               return <div>{ability.ability.name}</div>;
+//             })}
+//           </div>
+//           {image ? <img src={image.sprites.front_default} /> : <div></div>}
+//         </div>
+//       ) : (
+//         <div>{loading && <PokeballLoadingSpinner />}</div>
+//       )}
+//     </div>
+//   );
 // }
 
-export default function PokemonService({ pokemon }) {
-  const [data, setData] = useState(null);
-  const [image, setImage] = useState(null);
-  const [loading, setLoading] = useState(false);
+// export default async function getAllData({ pokemon }) {
+//   let url = pokemon.toLowerCase();
 
-  async function getAllData() {
-    setLoading(true);
-    console.log(loading);
+//   let resData = await api.get(url);
 
-    let url = pokemon.toLowerCase();
+//   let imageData = await api.get(resData.data.forms[0].url);
 
-    let resData = await api.get(url);
+//   let allData = { data: resData, image: imageData };
 
-    let imageData = await api.get(resData.data.forms[0].url);
+//   return allData;
+// }
 
-    setData(resData.data);
-    setImage(imageData.data);
-
-    setLoading(false);
-    console.log(loading);
-  }
-
-  // async function fetchPokemon() {
-  //   let url = pokemon.toLowerCase();
-
-  //   await api.get(url).then((res) => {
-  //     setData(res.data);
-  //     return res.data;
-  //   });
-  // }
-
-  // async function fetchPokemonImage(resData) {
-  //   await api.get(resData.forms[0].url).then((res) => {
-  //     setImage(res);
-  //     return res.data;
-  //   });
-  // }
-
-  return (
-    <div>
-      <Button
-        variant="contained"
-        onClick={getAllData}
-        className="search-button"
-      >
-        Search
-      </Button>
-      {data ? (
-        <div className="pokemon-card">
-          <div className="pokemon-name">{data.name}</div>
-          <div className="ability-container">
-            {data.abilities.map((ability) => {
-              console.log(ability);
-              return <div>{ability.ability.name}</div>;
-            })}
-          </div>
-          {image ? <img src={image.sprites.front_default} /> : <div></div>}
-        </div>
-      ) : (
-        <div>{loading && <PokeballLoadingSpinner />}</div>
-      )}
-    </div>
-  );
-}
+// export { getAllData };
--- pokedex-frontend/src/view/DefaultView.js
+++ pokedex-frontend/src/view/DefaultView.js
@@ -1,12 +1,118 @@
-import React, { Component } from "react";
-import PokedexLayout from "../components/PokedexLayout";
+import React, { useState, Fragment } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import PokeballLoadingSpinner from "../components/PokeballLoadingSpinner";
+import axios from "axios";
 
-export default class DefaultView extends Component {
-  render() {
+const api = axios.create({
+  baseURL: "https://pokeapi.co/api/v2/pokemon/",
+});
+
+export default function DefaultView(props) {
+  const [pokemonName, setPokemonName] = useState("");
+  const [data, setData] = useState(null);
+  const [image, setImage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [showMoves, setShowMoves] = useState(false);
+
+  async function getAllData() {
+    setLoading(true);
+    let url = pokemonName.toLowerCase();
+
+    let resData = await api.get(url);
+
+    let imageData = await api.get(resData.data.forms[0].url);
+
+    setData({ generalData: { ...resData.data, ...imageData.data } });
+
+    setLoading(false);
+  }
+
+  function getAbilities() {
     return (
-      <div className="default-layout">
-        <PokedexLayout />
+      <div className="ability-container">
+        {data.generalData.abilities.map((ability, index) => {
+          return <div key={index}>{formatText(ability.ability.name)}</div>;
+        })}
       </div>
     );
   }
+
+  function getSprites() {
+    return (
+      <div className="sprite-container">
+        {data.generalData &&
+          Object.keys(data.generalData.sprites)
+            .reverse()
+            .map((sprite, index) => {
+              if (data.generalData.sprites[sprite] !== null) {
+                return (
+                  <div key={index}>
+                    <img src={data.generalData.sprites[sprite]} alt="Pokemon" />
+                    <div className="sprite-name">{formatText(sprite)}</div>
+                  </div>
+                );
+              }
+            })}
+      </div>
+    );
+  }
+
+  function searchContainer() {
+    return (
+      <div className="search-container">
+        <TextField
+          id="outlined-basic"
+          label="Enter Pokemon name"
+          variant="outlined"
+          onChange={(event) => setPokemonName(event.target.value)}
+          className="pokemon-input-name"
+        />
+        <Button variant="contained" onClick={getAllData}>
+          Search
+        </Button>
+      </div>
+    );
+  }
+
+  function formatText(text) {
+    let newString = text;
+    newString = text.replaceAll("-", " ");
+    newString = newString.replaceAll("_", " ");
+    return newString;
+  }
+
+  function getMoves() {
+    return (
+      <div>
+        {data.generalData.moves.map((move, index) => {
+          return (
+            <div key={index} className="move-container">
+              {formatText(move.move.name)}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+
+  return (
+    <div className="default-layout">
+      {searchContainer()}
+      {data ? (
+        <div className="pokemon-card">
+          <div className="pokemon-name">{data.generalData.name}</div>
+          <br />
+          {getAbilities()}
+          {getSprites()}
+          <Button variant="contained" onClick={() => setShowMoves(!showMoves)}>
+            Moves
+          </Button>
+          {showMoves === false ? <Fragment></Fragment> : getMoves()}
+        </div>
+      ) : (
+        <div>{loading && <PokeballLoadingSpinner />}</div>
+      )}
+    </div>
+  );
 }
